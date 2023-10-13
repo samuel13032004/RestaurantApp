14a56d8242604381dbd2c23807869446dc4493d1
samueldeniz <sdenizsantana@gmail.com>
2023-10-13 10:03:56 +0100

    class orderitem commit

--- src/control/Main.java
+++ src/control/Main.java
@@ -2,6 +2,22 @@ package control;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //i. Init. Crear dos clientes.
+        //ii. Init. Crear dos restaurantes. (*) En el caso que el restaurante tenga un número de teléfono no válido se almacenará el valor: “XXXX”.
+        //iii. Init. Crear tres platos diferentes.
+        //iv. Imprimir por pantalla los datos del primer restaurante.
+        //v. Imprimir por pantalla los datos del segundo cliente.
+        //vi. Imprimir por pantalla los datos del tercer plato.
+        //vii. Crear un pedido por parte del primer cliente, al primer restaurante, que
+        //incluya los dos últimos platos del catálogo. (*) Sugerencia: para especificar los
+        platos y la cantidad de ellos, se pueden utilizar dos ArrayList<Integer> que indiquen la
+        posición del plato en el catálogo y la cantidad de dicho plato, respectivamente.
+        //(void order(Customer customer, Restaurant restaurant, ArrayList<Integer> dishesId, ArrayList<Integer>
+                quantity))
+        //viii. Borrar el segundo cliente.
+        //ix. Imprimir por pantalla el número de clientes.
+        //x. Imprimir por pantalla el primer pedido del primer cliente, incluyendo el precio total del pedido.
+
+
     }
 }
--- src/model/OrderItem.java
+++ src/model/OrderItem.java
@@ -1,4 +1,17 @@
 package model;
 
 public class OrderItem {
+    private int quantity;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public OrderItem(int quantity){
+        this.quantity = quantity;
+    }
 }
