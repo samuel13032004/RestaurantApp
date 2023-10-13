d55723c733550947b0f93b995c0855f9d2c79354
Guedes <albertoguedes4@gmail.com>
2023-10-13 10:26:51 +0100

    class commit

--- src/org/ulpgc/is1/model/Restaurant.java
+++ src/org/ulpgc/is1/model/Restaurant.java
@@ -1,5 +1,28 @@
 package org.ulpgc.is1.model;
 
 public class Restaurant {
+	private String name;
+	private Phone phone;
 
+	public Restaurant(String name, Phone phone){
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Phone getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Phone phone) {
+		this.phone = phone;
+	}
+	//TODO addMenu()
 }
