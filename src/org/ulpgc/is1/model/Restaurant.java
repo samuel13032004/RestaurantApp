9e195e34a946052ad65cc64e720b29985e2a4ee6
samueldeniz <sdenizsantana@gmail.com>
2023-10-15 12:25:58 +0100

    commit Restaurant

--- src/org/ulpgc/is1/model/Restaurant.java
+++ src/org/ulpgc/is1/model/Restaurant.java
@@ -1,17 +1,12 @@
 package org.ulpgc.is1.model;
 
-import java.util.ArrayList;
-
 public class Restaurant {
 	private String name;
 	private Phone phone;
 
-	private ArrayList<Menu>menus;
-
 	public Restaurant(String name, Phone phone){
 		this.name = name;
 		this.phone = phone;
-		this.menus = new ArrayList<Menu>();
 	}
 
 	public String getName() {
@@ -29,12 +24,5 @@ public class Restaurant {
 	public void setPhone(Phone phone) {
 		this.phone = phone;
 	}
-
-	public Menu getMenu(int i){
-		return this.menus.get(i);
-	}
-
-	public void addMenu(Menu newMenu){
-		this.menus.add(newMenu);
-	}
+	//TODO addMenu()
 }
