1b894b7e1b9d0421c8f091dc1e637f58b9406ce0
samueldeniz <sdenizsantana@gmail.com>
2023-10-15 11:58:26 +0100

    commit Order

--- src/org/ulpgc/is1/model/Order.java
+++ src/org/ulpgc/is1/model/Order.java
@@ -6,12 +6,17 @@ public class Order {
     private static int NEXT_ID = 0;
     private final int id;
 
+    private Customer customer;
+    private ArrayList<Customer> c:
     private ArrayList<OrderItem> orderItem;
     public void addOrderItem(int quantity, Dish dish) {
         OrderItem orderItem1 = new OrderItem(quantity, dish);
         orderItem.add(orderItem1);
     }
-    public Order() {
+    public Order(Customer customer) {
         this.id = NEXT_ID++;
+        this.c = new ArrayList<Customer>();
+        this.customer = new Customer();
+
     }
 }
